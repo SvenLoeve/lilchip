48d957d13c122604ddb951b5be43aeaa8004ab48
Hiddeeeh <hidde.linderse@gmail.com>
2023-10-26 12:02:27 +0200

    First commit

---
+++ lilchip/src/components/Login/Login.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Login.css'
+
+const Login = () => {
+    return (
+        <div className='container'>
+            <div className='header'>
+                <div className='text'>Log in</div>
+            </div>
+            <div className='input'>
+                <input type='text' placeholder='Username' />
+            </div>
+            <div className='header'>
+                <div className='text'>Password</div>
+            </div>
+            <div className='input'>
+                <input type='password' placeholder='Password' />
+            </div>
+            <div className='buttons'>
+                <button>cancel</button>
+                <button>Login</button>
+            </div>
+        </div>
+    );
+}
+
+export default Login
