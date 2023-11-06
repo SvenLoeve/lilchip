ad78cd81d8d8738c094743b698f8b0f9a40340cb
Hiddeeeh <hidde.linderse@gmail.com>
2023-11-06 11:36:12 +0100

    added variable inputs and startet error handeling

--- lilchip/src/components/Login/Login.jsx
+++ lilchip/src/components/Login/Login.jsx
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css'
 
 const Login = () => {
+    const [input, setInput] = useState({
+        username: "",
+        password: "",
+    });
+
+    const [errors, setErrors] = useState({});
+
+    const handlechange = (event) => {
+        const { name, value } = event.target;
+        setInput({
+            ...input,
+            [name]: value,
+        })
+    }
+
     return (
         <div className='container'>
             <div className='header'>
                 <div className='text'>Log in</div>
             </div>
             <div className='input'>
-                <input type='text' placeholder='Username' />
+                <input type='text' name='username' value={input.username} onChange={handlechange} placeholder='Username' />
             </div>
             <div className='header'>
                 <div className='text'>Password</div>
             </div>
             <div className='input'>
-                <input type='password' placeholder='Password' />
+                <input type='password' name='password' value={input.password} onChange={handlechange} placeholder='Password' />
             </div>
             <div className='buttons'>
                 <button className='button back'>cancel</button>
